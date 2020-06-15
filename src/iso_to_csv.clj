78d0628e4e25c6c59f8dd6e5e86b106e07327ee6
Christian Egli <christian.egli@sbs.ch>
2020-06-15 22:31:02 +0200

    Add initial and final balance to the csv output

--- src/iso_to_csv.clj
+++ src/iso_to_csv.clj
@@ -2,10 +2,17 @@
   (:require [clojure.data.csv :as csv]
             [iso-to-ledger :as ledger]))
 
+(defn- add-balances
+  [entries initial-balance final-balance]
+  (let [balances (concat [initial-balance] (repeat (- (count entries) 2) nil) [final-balance])]
+    (map (fn [entry bal] (assoc entry :balance bal)) entries balances)))
+
 (defn -main
   "Convert [ISO 20022](https://en.wikipedia.org/wiki/ISO_20022) to CSV"
   [& args]
   (doseq [file args]
-    (let [entries (-> file ledger/read-file :entries)
+    (let [data (-> file ledger/read-file)
+          [initial-balance final-balance] map :amount (:balance data)
+          entries (add-balances (:entries data) initial-balance final-balance)
           header (->> entries first keys (map name))]
       (csv/write-csv *out* (conj (->> entries (map vals)) header)))))
